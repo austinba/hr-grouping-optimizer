02f7b265f8c87b52e3134ca6ec6567ef3038d5a1
Austin Baltes <Austin Baltes>
2016-05-19 22:53:23 -0700

    Updated Console Log Formatting and Created Main Function to Run Optimizer Multiple Times

--- main.js
+++ main.js
@@ -1,25 +1,42 @@
 'use strict';
 const util = require('./util');
 const objFn = require('./objectiveFunction');
-const students = require('./exampleStudentLists/relaxed.json')
 
-let _incTemps = [];  // used only to 
-// annealing parameters: optimize these by checking at what temperatures bests are being hit
-const startTemp = 3.5;
-const endTemp = .05;
+// Annealing Parameters
+const startTemp = 350;
+const endTemp = 5;
 const iterations = 100000;
+const runCount = 10;
+let _incTemps = [];  // tracker for temps where improvements happened -- for optimizing paraameters
+
+
+const students = require('./exampleStudentLists/relaxed.json')
 const groupSize = 4;
 
-// OPTIMIZER
-const optimizer = function() {
+const main = function(runCount, students, groupSize) {
+	let groupingOptions = [];
+	for(let i=0; i<runCount; i++) {
+		console.log('Generating Grouping Option #' + i)
+		groupingOptions.push(optimizer(students, groupSize));
+		console.log('\n');
+	}
+	groupingOptions = groupingOptions.sort((groupingA, groupingB) => objFn.forAGroupConfig(groupingA)<objFn.forAGroupConfig(groupingB));
+	console.log('Points:', groupingOptions.map(grouping => objFn.forAGroupConfig(grouping)));
+}
+
+// OPTIMIZER inputs: 'students' array of preferences and 'groupSize'
+const optimizer = function(students, groupSize) {
+
 	// Start with a completely randomized group configuration
-  const groupConfig = randomizeGroupConfig(students);
+  const groupConfig = randomizeGroupConfig(students, groupSize);
 
   // Initialize variables for annealing / optimization
 	let bestConfig = util.deepCopyArray(groupConfig);
 	let bestScore = objFn.forAGroupConfig(bestConfig);
 	const decrement = Math.exp(Math.log(startTemp / endTemp) / iterations);
 
+	// Loop while randomly swapping students and decreasing temperature (represents probability of taking a sub-optimal swap)
+	process.stdout.write('Current Best Score:  ');
 	for(let i=0; i<iterations; i++) {
 		const currentTemp = startTemp / Math.pow(decrement, i);
 		randomlySwapTwoStudentsFromDifferentGroups(groupConfig, currentTemp);
@@ -30,20 +47,25 @@ const optimizer = function() {
 			bestScore = score;
 			bestConfig = util.deepCopyArray(groupConfig);
 			_incTemps.push(currentTemp);
-			console.log(score, currentTemp);
+			process.stdout.write(score + '(' + Math.round(currentTemp) + '°)  ');
 		} 
 	}
 	// For Optimizing Annealing Parameters: Output temperatures where improvements happened
 	// console.log(JSON.stringify(__incTemps))
-	return bestScore;
+	return bestConfig;
 };
 
-const randomizeGroupConfig = function(students) {
-// generates an initial random group configuration from an array of students
-  let unassignedStudents = students.slice();  // copy students array
+
+//--------------------------------
+//TODO:  Do not allow swaps that put two undefineds in the same group
+//--------------------------------
+
+// RANDOMIZE GROUP CONFIG Creates an initial group configuration using students list
+const randomizeGroupConfig = function(students, groupSize) {
+  let unassignedStudents = students.slice();
 	const groupCount = Math.ceil(students.length / groupSize);
 
-	// create 2D array and populate
+	// Create 2D array[groupID][studentInGroupIndex] and populate with students
   const groupConfiguration = util.range(groupCount).map(() => []);
 	for (let j=0; j<groupSize; j++) {
 		for (let i=0; i<groupCount && i*j<students.length; i++) {
@@ -54,31 +76,34 @@ const randomizeGroupConfig = function(students) {
 	return groupConfiguration;
 }
 
+// RANDOMLY SWAP TWO STUDENTS FROM DIFFERENT GROUPS Tests a swap and saves it if 
 const randomlySwapTwoStudentsFromDifferentGroups = function(groupConfiguration, currentTemp) {
 	const groupCount = groupConfiguration.length;
-	const group1 = util.randInt(groupCount);
-	let group2 = util.randInt(groupCount - 1);
-	group2 = group2<group1 ? group2 : group2+1; // choice2 !== choice1
-	const student1 = util.randInt(groupSize);
-	const student2 = util.randInt(groupSize);
 
-	// check base case points
+	// Randomly select the two groups, and the two students within those groups to swap
+	let group1 = util.randInt(groupCount);
+	let group2 = util.randInt(groupCount - 1); // group 2 should not be the same as group 1
+	    group2 = group2<group1 ? group2 : group2+1;
+	const student1 = util.randInt(groupConfiguration[group1].length);
+	const student2 = util.randInt(groupConfiguration[group2].length);
+
+	// Check points count before swapping
 	const baseCasePoints = objFn.forAGroup(groupConfiguration[group1]) +
 		                     objFn.forAGroup(groupConfiguration[group2]);
 
-	// swap students
+	// Swap the students
 	let temp = groupConfiguration[group1][student1];
 	groupConfiguration[group1][student1] = groupConfiguration[group2][student2];
   groupConfiguration[group2][student2] = temp;
 
 
-  // check points in both groups
-	const newCasePoints = objFn.forAGroup(groupConfiguration[group1]) +
+  // Check points count after swapping
+	const testCasePoints = objFn.forAGroup(groupConfiguration[group1]) +
 		                     objFn.forAGroup(groupConfiguration[group2]);
 
-  // unswap if not desired
-  if (newCasePoints < baseCasePoints && // keep all where better
-  	  Math.random() > Math.exp(-(baseCasePoints-newCasePoints)/currentTemp)) // keep sometimes when worse, depending on how much worse and temperature
+  // Unswap if configuration is disadvantaged AND a high temperature does not override decision
+  if (testCasePoints < baseCasePoints &&
+  	  Math.random() > Math.exp(-(baseCasePoints-testCasePoints)*100/currentTemp))
   {
 		let temp = groupConfiguration[group1][student1];
 	  groupConfiguration[group1][student1] = groupConfiguration[group2][student2];
@@ -86,8 +111,5 @@ const randomlySwapTwoStudentsFromDifferentGroups = function(groupConfiguration, 
   }
 }
 
-optimizer();
-
-// let res = util.range(50).map(() => main2());
-// console.log(res);
-// console.log('average:', res.reduce((memo, val) => memo + val)/2);
+// Run the optimizer
+main(runCount, students, groupSize);
